574eb1c04a5ae72dca2703538abe93c8242d9e34
keyanbagheri <keyanbagheri@gmail.com>
2014-06-15 19:19:33 -0400

    tweaking java ajax on new event form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,10 @@
 class EventsController < ApplicationController
+
+  respond_to :json
+
   def index
-    @events = Event.all
+    events = Event.all
+    respond_with events
   end
 
   def show
@@ -8,17 +12,30 @@ class EventsController < ApplicationController
   end
 
   def new
+    @event = Event.find_by_id(params[:id])
+    # change this later
   end
 
   def create
+    event = Event.create(event_params)
+    respond_with event
   end
 
   def edit
   end
 
   def update
+    event = Event.find(params[:id])
+    event.update(event_params)
+    respond_with event
   end
 
   def delete
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:id, :name, :blurb, :info)
   end
 end
