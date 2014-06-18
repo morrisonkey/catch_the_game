9eceb6eb94d2fe35f157254c9cb0114187dab025
kssweet1 <kssweet@gmail.com>
2014-06-18 19:35:43 -0400

    LOST CAUSE ON THE DISPLAY EVENTS/updated css

--- app/assets/javascripts/broadcast.js
+++ app/assets/javascripts/broadcast.js
@@ -112,47 +112,10 @@ var theMostIntheFutureBroadcastsView = new CollectionOfBroadcastCollectionViews(
 
 $(document).ready(function() {
 
-
-
-  $(todaysBroadcasts).on("fetch-complete", function(){
     
      // debugger
     $(".forever_scroll").append(todaysBroadcastsView.render().el);
 
-
-    
-  $('#cssmenu > ul > li ul').each(function(index, element){
-      var count = $(element).find('li').length;
-      var content = '<span class="cnt">' + count + '</span>';
-      $(element).closest('li').children('a').append(content);
-    });
-
-    $('#cssmenu ul ul li:odd').addClass('odd');
-    $('#cssmenu ul ul li:even').addClass('even');
-
-    $('#cssmenu > ul > li > a').click(function() {
-
-      var checkElement = $(this).next();
-
-      $('#cssmenu li').removeClass('active');
-      $(this).closest('li').addClass('active'); 
-
-      if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
-        $(this).closest('li').removeClass('active');
-        checkElement.slideUp('normal');
-      }
-      if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
-        $('#cssmenu ul ul:visible').slideUp('normal');
-        checkElement.slideDown('normal');
-      }
-
-      if($(this).closest('li').find('ul').children().length == 0) {
-        return true;
-      } else {
-        return false; 
-      }
-
-    });
 
 
   });
